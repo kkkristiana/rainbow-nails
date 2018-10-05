ada92d4d7015b97b5f0dcd417cc5693ee18115e6
Kristiana Atasi <kristiana@evori.com>
2018-10-05 14:34:02 +0300

    Adding project files

---
+++ RainbowNails/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RainbowNails
+//
+//  Created by kristianaatasi on 5.10.18.
+//  Copyright © 2018 com.ka.rainbow-nails. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
